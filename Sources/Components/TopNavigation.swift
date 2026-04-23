21d1b529899a77f2fc0702b47a0e721c7a175914
shiinayane <shiinayane@gmail.com>
2026-04-23 20:25:02 +0900

    fix: use trailing-slash nav routes

--- Sources/Components/TopNavigation.swift
+++ Sources/Components/TopNavigation.swift
@@ -4,7 +4,7 @@ import Raptor
 func TopNavigation() -> Navigation {
     Navigation {
         Link("Home", destination: "/")
-        Link("Archive", destination: "/archive")
-        Link("About", destination: "/about")
+        Link("Archive", destination: "/archive/")
+        Link("About", destination: "/about/")
     }
 }
