42c4df6f2b7666ae9115cdd9563a8ccac304668d
shiinayane <shiinayane@gmail.com>
2026-04-26 21:02:49 +0900

    fix: align page canvas background

--- Sources/Styles/Visual/PageCanvasStyle.swift
+++ Sources/Styles/Visual/PageCanvasStyle.swift
@@ -7,13 +7,13 @@ struct PageCanvasStyle: Style {
 
         if environment.horizontalSizeClass < .regular {
             content
-                .background(palette.canvasBackground)
+                .background(palette.pageBackground)
                 .style(.paddingBlock(.px(24)))
                 .style(.paddingInline(.px(16)))
                 .style(.minWidth(.px(0)))
         } else {
             content
-                .background(palette.canvasBackground)
+                .background(palette.pageBackground)
                 .style(.paddingBlock(.px(40)))
                 .style(.paddingInline(.px(24)))
                 .style(.minWidth(.px(0)))
